e346181bc8cdaa332238b0719e63624ec6cb02c7
tak4822 <tkc4822@gmail.com>
2017-12-10 18:21:54 -0800

    merge tom's section

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 'use strict'
 
 var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
   prefix = require('gulp-autoprefixer'),
     maps = require('gulp-sourcemaps');
 
+function swallowErr (err) {
+    console.log( err.toString());
+    this.emit('end');
+}
+
 gulp.task('compileSass', function() {
   return gulp.src("scss/style.scss")
+      .pipe(plumber({
+          errorHandler: swallowErr
+      }))
       .pipe(maps.init())
       .pipe(sass())
       .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
