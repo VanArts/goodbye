c198c3532f2cb75ac0109f5389455edbca71a059
tak4822 <tkc4822@gmail.com>
2017-12-10 21:06:26 -0800

    add tak

--- js/web12.js
+++ js/web12.js
@@ -4,5 +4,20 @@ $(document).ready(function() {
 		navigationPosition: 'right',
 		navigationTooltips: ['home', 'Haruna','Tak','Estefania', 'Carly', 'Tom', 'Nishanth'
 	, 'Sandy', 'Nina', 'Peter', 'Coleman', 'Sachiko', 'Michael'],
+
+	afterLoad: function(anchorLink, index){
+		var loadedSection = $(this);
+
+		//tak section
+		if(index == 3){
+			$('#tak').find('.face').addClass('jackInTheBox').css('opacity',1);
+			setTimeout(function(){
+				$('#tak').find('.bubble').addClass('jackInTheBox').css('opacity',1);
+			}, 500);
+		}
+
+
+	}
 	});
+
 });
